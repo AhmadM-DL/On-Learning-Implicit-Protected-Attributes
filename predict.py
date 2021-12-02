f888159216ce27af8ec655328a2f62acc7c0c720
Ahmad Mustapha <amm90@mail.aub.edu>
2021-12-02 23:49:27 +0200

    test to predict

--- predict.py
+++ predict.py
@@ -24,8 +24,8 @@ def prepare_split_dataset(dataset_path, split_path):
 
   return train_df, validation_df, test_df
 
-def test(dataset, split_path, model_name, pretrain_model_path,
-         batch_size= 32, class_mode= "raw", height=320, width=320):
+def predict(dataset, split_path, model_name, pretrain_model_path, tag="test_predict_probability",
+         output_dir="./", batch_size= 32, class_mode= "raw", height=320, width=320):
   # Preparing Datasets
   if "chexpert" in dataset.lower():
     img_root_dir = "./Datasets/Chexpert/"
@@ -38,6 +38,8 @@ def test(dataset, split_path, model_name, pretrain_model_path,
   else:
     raise Exception("Not supported dataset")
   _, _, test_df = prepare_split_dataset(dataset_path, split_path)
+  test_df = test_df.iloc[:100, :]
+  
   # Load model
   _, preprocess_input = Classifiers.get(model_name)
   # Load and Set model
@@ -53,49 +55,30 @@ def test(dataset, split_path, model_name, pretrain_model_path,
                                                     shuffle= False,
                                                     batch_size= batch_size)
   # test_epoch = math.ceil(len(test_df) / batch_size)
-  results = model.evaluate(test_batches)
-  return results
+  predictions = model.predict(test_batches)
+  results_groundtruth = pd.DataFrame(
+    np.concatenate([predictions, test_df.iloc[:, 1:].values], axis=1),
+    columns= test_df.columns[1:].tolist() + [c+"_gt" for c in test_df.columns[1:]]
+  )
+  results_groundtruth.to_csv(os.path.join(output_dir, tag+".csv"))
+  return
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='A module to train models')
 
-  parser.add_argument('--dataset_path', type=str, help='')
-  parser.add_argument('--split_path', type=str, help='')
-  parser.add_argument("--output_dir", type=str, help='')
-  parser.add_argument('--img_root_dir', type=str, help='')
-  parser.add_argument('--tag', type= str, default = "v1",  help='')
+  parser = argparse.ArgumentParser(description='A module to infere data from models')
 
-  parser.add_argument('--height', type=int, default=320, help='')
-  parser.add_argument('--width', type=int, default=320, help='')
-  parser.add_argument('--model', type=str, choices= Classifiers.models_names())
-  parser.add_argument('--seed', type=int, default= 0, help='')
-  parser.add_argument('--weights', type=str, default= None, help='')
-  parser.add_argument('--n_labels', type=int, help='')
-  parser.add_argument('--multilabel', type=bool, help='')
-
-  # Training Configuration
-  parser.add_argument('--learning_rate', type=float, default= 1e-3,  help='')
-  parser.add_argument('--momentum_val', type=float, default=0.9, help='')
-  parser.add_argument('--decay_val', type=float, default=0.0, help='')
+  parser.add_argument('--dataset', type=str, help='')
+  parser.add_argument('--split_file', type=str, help='')
+  parser.add_argument('--model_name', type=str, choices= Classifiers.models_names())
+  parser.add_argument('--pretrain_model_path', type=str)
+  parser.add_argument('--tag', type= str)
+  parser.add_argument("--output_dir", type=str)
   parser.add_argument('--batch_size', type=int, default= 16, help='')
-
-  # Transformations
-  parser.add_argument('--rotation_range', type=int, default=15,  help='')
-  parser.add_argument('--fill_mode', type=str, default="constant", help='')
-  parser.add_argument('--horizontal_flip', type=bool, default=True, help='')
-  parser.add_argument('--crop_to_aspect_ratio', type=bool, default=True, help='')
-  parser.add_argument('--zoom_range', type=float, default=0.1, help='')
-
   parser.add_argument('--class_mode', type=str, default="raw", choices = ["categorical", "raw"], help='')
-  parser.add_argument('--freeze', type=int, help='')
-  parser.add_argument('--resume', action= "store_true",  help='')
+  parser.add_argument("--height")
+  parser.add_argument("--width")
 
   args = parser.parse_args()
 
-  train(args.dataset_path, args.split_path, args.img_root_dir, args.tag,
-            args.height, args.width, args.model, args.seed, args.weights, args.n_labels,
-            args.freeze, args.resume, args.output_dir, args.multi_label,
-            learning_rate= args.learning_rate, momentum_val=args.momentum_val, decay_val=args.decay_val,
-            batch_size= args.batch_size, rotation_range= args.rotation_range, fill_mode= args.fill_mode,
-            horizontal_flip= args.horizontal_flip, crop_to_aspect_ratio= args.crop_to_aspect_ratio,
-            zoom_range= args.zoom_range, class_mode= args.class_mode)
+  predict(args.dataset, args.split_file, args.model_name, args.pretrain_model_path,
+          args.tag, args.output_dir, args.batch_size, args.class_mode, args.height, args.width)
